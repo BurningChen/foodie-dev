2fedfcb4ac72adcd1d9eacac9b54d3b3fb12c5a0
BurningIce <13025540813@163.com>
2021-06-08 17:40:52 +0800

    2.0.0

--- foodie-dev-api/src/main/java/com/imooc/WarStarterApplication.java
+++ foodie-dev-api/src/main/java/com/imooc/WarStarterApplication.java
@@ -1,9 +1,9 @@
-//package com.imooc;
-//
+package com.imooc;
+
 //import org.springframework.boot.builder.SpringApplicationBuilder;
 //import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 //
-//// 打包war [4] 增加war的启动类
+// 打包war [4] 增加war的启动类
 //public class WarStarterApplication extends SpringBootServletInitializer {
 //
 //    @Override
